5d5ea8ddf3d77eb4c54ae1d5103b528b2842a379
n-bhargava <67077159+n-bhargava@users.noreply.github.com>
2024-03-03 11:43:21 -0800

    Update ViewController.swift

--- athenahacks/ViewController.swift
+++ athenahacks/ViewController.swift
@@ -6,14 +6,92 @@
 //
 
 import UIKit
+import CoreLocation
+//import MessageUI
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var emergencyButton: UIButton!
+    
+    @IBOutlet weak var bellButton: UIButton!
+    
+    @IBOutlet weak var starButton: UIButton!
+    @IBOutlet weak var heartButton: UIButton!
+    
+    var manager = CLLocationManager()
+    
+    var text = ""
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let first = locations.first else {
+            return
+        }
+        
+        text = "Please help me at this location: \(first.coordinate.longitude) | \(first.coordinate.latitude)"
+    }
+    
+    
+    
+    @IBAction func sendMessage(_ sender: Any) {
+        let phoneNumber = "9169450540"
+        
+        
+        guard let messageURL = NSURL(string: "sms:\(phoneNumber)&body=\(text)")
+        else {return}
+        
+        if UIApplication.shared.canOpenURL(messageURL as URL) {
+            UIApplication.shared.openURL(messageURL as URL)
+        }
+        
+    }
+    
+    @IBAction func sendMessageBell(_ sender: Any) {
+        let phoneNumber = "9169450540"
+        
+        
+        guard let messageURL = NSURL(string: "sms:\(phoneNumber)&body=\(text)")
+        else {return}
+        
+        if UIApplication.shared.canOpenURL(messageURL as URL) {
+            UIApplication.shared.openURL(messageURL as URL)
+        }
+    }
+    
+    @IBAction func sendMessageStar(_ sender: Any) {
+        let phoneNumber = "9169450540"
+        
+        
+        guard let messageURL = NSURL(string: "sms:\(phoneNumber)&body=\(text)")
+        else {return}
+        
+        if UIApplication.shared.canOpenURL(messageURL as URL) {
+            UIApplication.shared.openURL(messageURL as URL)
+        }
+    }
+    
+    @IBAction func sendMessageHeart(_ sender: Any) {
+        let phoneNumber = "9169450540"
+        
+        guard let messageURL = NSURL(string: "sms:\(phoneNumber)&body=\(text)")
+        else {return}
+        
+        if UIApplication.shared.canOpenURL(messageURL as URL) {
+            UIApplication.shared.openURL(messageURL as URL)
+        }
+    }
+    
 }
-
